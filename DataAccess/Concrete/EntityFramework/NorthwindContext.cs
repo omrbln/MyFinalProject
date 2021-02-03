effe0fe0fdd81da9e60740e09505190223864e03
ÖMER BİLEN <omer.bilen44@hotmail.com>
2021-02-03 23:46:30 +0300

    3 Şubat Çarşamba Ders Sonu...

---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context : Db tabloları ile proje class larını bağlamak
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@"Server=175.45.2.12");  ==>  Gerçek işlerde bu şekilde IP adresi yer alır
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
