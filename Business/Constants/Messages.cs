5fb1afd1ec40f6100dcd8e530907f291825b7e8d
ÖMER BİLEN <omer.bilen44@hotmail.com>
2021-02-11 00:04:07 +0300

    10 Şubat Çarşamba Ders Sonu...

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    //static yapılar new lenmez. Direk masseges. diyoruz uygulama hayatı boyunca tek instance oluyor
+    public static class Messages
+    {
+        //public olduğu için PascalCase yazıyoruz.
+        public static string ProductAdded = "Ürün eklendi.";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda.";
+        public static string ProductsListed = "Ürünler listelendi.";
+    }
+}
